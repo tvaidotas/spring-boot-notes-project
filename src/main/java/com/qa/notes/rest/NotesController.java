75010703682790f333a9656f0ef663fcdf47a425
tvaidotas <tadas.vaidotas@gmail.com>
2020-08-26 15:27:45 +0100

    added a create endpoint

--- src/main/java/com/qa/notes/rest/NotesController.java
+++ src/main/java/com/qa/notes/rest/NotesController.java
@@ -4,6 +4,8 @@ import com.qa.notes.domain.Note;
 import com.qa.notes.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +23,11 @@ public class NotesController {
     @GetMapping("/")
     public List<Note> getAllNotes(){
         return this.noteService.readAllNotes();
+    }
+
+    @PostMapping("/createNote")
+    public Note createNote(@RequestBody Note note){
+        return this.noteService.createNote(note);
     }
 
 }
