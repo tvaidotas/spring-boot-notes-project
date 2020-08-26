c9fcb27240c3d5384e8ff60589b7797e112e863c
tvaidotas <tadas.vaidotas@gmail.com>
2020-08-26 14:37:14 +0100

    added CRUD functionality to the service

--- src/main/java/com/qa/notes/service/NoteService.java
+++ src/main/java/com/qa/notes/service/NoteService.java
@@ -1,6 +1,7 @@
 package com.qa.notes.service;
 
 import com.qa.notes.domain.Note;
+import com.qa.notes.exceptions.NoteNotFoundException;
 import com.qa.notes.repo.NotesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +27,22 @@ public class NoteService {
     }
 
     public Note findNoteById(Long id){
-        return this.repo.findById(id).orElseThrow();
+        return this.repo.findById(id).orElseThrow(NoteNotFoundException::new);
+    }
+
+    public Note updateNote(Long id, Note note){
+        Note update = findNoteById(id);
+        update.setTitle(note.getTitle());
+        update.setDescription(note.getDescription());
+        return this.repo.save(update);
+    }
+
+    public Boolean deleteNoteById(Long id){
+        if(!this.repo.existsById(id)){
+            throw new NoteNotFoundException();
+        }
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
     }
 
 }
