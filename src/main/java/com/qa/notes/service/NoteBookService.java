c0b4691c59bb9ac70eede3b5c30bef4b66694cbd
tvaidotas <tadas.vaidotas@gmail.com>
2020-08-27 11:13:20 +0100

    added notebook service crud functionality

--- src/main/java/com/qa/notes/service/NoteBookService.java
+++ src/main/java/com/qa/notes/service/NoteBookService.java
@@ -1,8 +1,13 @@
 package com.qa.notes.service;
 
+import com.qa.notes.domain.NoteBook;
+import com.qa.notes.exceptions.NoteBookNotFoundException;
+import com.qa.notes.exceptions.NoteNotFoundException;
 import com.qa.notes.repo.NoteBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NoteBookService {
@@ -14,8 +19,30 @@ public class NoteBookService {
         this.repo = repo;
     }
 
+    public List<NoteBook> readAllNoteBooks(){
+        return this.repo.findAll();
+    }
 
+    public NoteBook createNoteBook(NoteBook noteBook){
+        return this.repo.save(noteBook);
+    }
 
+    public NoteBook findNoteBookById(Long id){
+        return this.repo.findById(id).orElseThrow(NoteBookNotFoundException::new);
+    }
 
+    public NoteBook updateNoteBook(Long id, NoteBook noteBook){
+        NoteBook update = findNoteBookById(id);
+        update.setName(noteBook.getName());
+        return this.repo.save(update);
+    }
+
+    public Boolean deleteNoteBookById(Long id){
+        if(!this.repo.existsById(id)){
+            throw new NoteNotFoundException();
+        }
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
+    }
 
 }
