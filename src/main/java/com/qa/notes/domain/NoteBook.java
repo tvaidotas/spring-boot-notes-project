5b57b406b4fec996cc6e2b9a1fe9fa4429421a9a
tvaidotas <tadas.vaidotas@gmail.com>
2020-08-27 11:03:56 +0100

    added the entity class for notebook

---
+++ src/main/java/com/qa/notes/domain/NoteBook.java
@@ -0,0 +1,40 @@
+package com.qa.notes.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class NoteBook {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    public NoteBook() {
+    }
+
+    public NoteBook(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
