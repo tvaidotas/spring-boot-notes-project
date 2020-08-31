ece38334d6c6f8435425f4d8a3625b27ccf240f2
tvaidotas <tadas.vaidotas@gmail.com>
2020-08-31 18:59:20 +0100

    added an example of GET for all notebooks and then displayed it on the page by manipulating the DOM

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -1,4 +1,4 @@
-function greeting(){
+function displayNotebooks(){
     const req = new XMLHttpRequest();
     req.onreadystatechange = () => {
         // Example handle logic
@@ -10,8 +10,27 @@ function greeting(){
                 // elem.textContent = "hello world";
                 // document.body.appendChild(elem);
 
+                let stuff = JSON.parse(req.response);
+                stuff.forEach(el => {
+                    // console.log(el); // prints whole element
+                    // console.log(el.name); // allows access to specific value
 
-
+                    // adding title to the body of the page
+                    let elem = document.createElement('div');
+                    let header = document.createElement('h1');
+                    header.textContent = "Notebook name: " + el.name;
+                    elem.appendChild(header);
+                    el.notes.forEach(note => {
+                        console.log(note) // print all notes for each notebook
+                        let title = document.createElement('p');
+                        let description = document.createElement('p');
+                        title.textContent = "Title: " + note.title;
+                        description.textContent = "Description: " + note.description;
+                        elem.appendChild(title);
+                        elem.appendChild(description);
+                    })
+                    document.body.appendChild(elem);
+                });
             } else {
                 console.log(
                     "Looks like its not JSON but lets see what it is... " + req.responseText
